32d7df71393eb6505ce8ac2d665a1ed0b91bd5c8
Robert L. Ryan <1085908428@qq.com>
2025-07-18 16:42:33 +0800

    Create sqrt2.c

---
+++ src/robert/sqrt2.c
@@ -0,0 +1,4 @@
+a=1e3,b,c=1413,d,f[1414],n=800,k;main(){for(;b<c;
+f[b++]=14);for(;n--;d+=*f*a,printf("%02d",d/a),*f
+=d%a)for(d=0,k=c;--k;d/=b,d*=2*k-1)f[k]=(d+=f[k]*
+a)%(b=100*k);}/* √2 2333 digits - Robert Ryan  */
