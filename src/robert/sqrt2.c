bd7735a07210e1d7fc92efd2a678eb89907db80d
Robert L. Ryan <1085908428@qq.com>
2025-07-18 16:49:01 +0800

    Update sqrt2.c

--- src/robert/sqrt2.c
+++ src/robert/sqrt2.c
@@ -1,3 +1,8 @@
+/* SQRT2 by Robert Ryan, 07-18-2025
+ * Calculate sqrt 2 to 2333 decimal digits(but no decimal point)
+ * output: 14142135623730950488168872429698078569...
+ */
+
 a=1e3,b,c=1413,d,f[1414],n=800,k;main(){for(;b<c;
 f[b++]=14);for(;n--;d+=*f*a,printf("%02d",d/a),*f
 =d%a)for(d=0,k=c;--k;d/=b,d*=2*k-1)f[k]=(d+=f[k]*
