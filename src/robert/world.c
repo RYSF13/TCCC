2db6ad0e44a73d96248ca898f5a2837fe3e59c9e
Robert L. Ryan <1085908428@qq.com>
2025-07-27 10:20:19 +0800

    Create world.c

---
+++ src/robert/world.c
@@ -0,0 +1,3 @@
+char*d="amaaaaobaaabgcipcaaopppopdaigppbaodampppbamdaopohaaalaohaaaaaibidahaaaidada\
+iaaaihadamdaaidacaaaaaibaaaaaaaaaaaaabaaaaaapphmkaaaipppphaa",i,j,k,l;main(){for(;i<
+15;i++,puts(""))for(j=0;j<9;j++)for(k=d[i*9+j]-97,l=0;l<4;k/=2,l++)putchar(32+k%2);}
